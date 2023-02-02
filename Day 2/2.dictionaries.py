f217c4a710c07fbbc0055a2b8a3642e8f62758dc
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-02-02 14:28:43 +0530

    Day-2 added

---
+++ Day 2/2.dictionaries.py
@@ -0,0 +1,17 @@
+student = {'name': 'Jhon', 'age': 25, 'courses': ['Math', 'CompSci']}
+for key, value in student.items():
+    print(key, value)
+
+
+
+
+
+#student['phone'] ='555-5555'
+#student['name'] = 'Jane'
+#print(student.get('phone'))
+#student.update({'name': 'Jane', 'age': 26, 'phone': '888-8888'})
+#del student['age']
+#age = student.pop('age')
+#print(student.values())
+#print(age)
+ 
---
+++ Day 2/3.conditionals.py
@@ -0,0 +1,36 @@
+condition = {}
+
+if condition: 
+    print('Evaluted to True')
+else:
+    print('Evaluated to False')
+
+
+
+
+
+# language ='JS'
+
+# if language == 'Python':
+#   print('Language is python')
+# elif language == 'JS':
+#    print('Language is JS')
+# else:
+#   print('No match')
+
+
+#user = 'Admin'
+#logged_in = False
+
+#if user == 'Admin' and logged_in:
+ #   print('Admin Page')
+#else:
+ #   print('Bad Creds')
+
+#a =[1,2,3]
+#b =[1,2,3]
+
+#print(id(a))
+#print(id(b))
+#print(a is b)
+#print(a == b)
