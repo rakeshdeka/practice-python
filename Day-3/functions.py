4bda2c8671d15179a5db3e42c736ce302556c478
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-02-05 21:11:33 +0530

    day3 added

---
+++ Day-3/functions.py
@@ -0,0 +1,27 @@
+# def hello_fun(greeting):
+#     return '{} Function.'.format(greeting)
+
+# print(hello_fun('Hi'))
+
+
+# print(hello_fun().upper())
+# print(len('Test'))
+
+# def calculateGmean(a,b):
+#     mean = (a*b)/(a+b)
+#     print(mean)
+
+# a=9
+# b=8
+
+# calculateGmean(a,b)
+
+# c=8
+# d=7
+
+# calculateGmean(c,d)
+
+def average(a=9, b=1):
+    print("The average is ", (a+b)/2)
+
+average(4)
---
+++ Day-3/loops.py
@@ -0,0 +1,22 @@
+#nums = [1,2,3,4,5]
+
+# for i in range(1,11):
+#     print(i)
+
+# for num in nums:
+#     if num == 3:
+#         print('Found')
+#         #break
+#         continue
+#     print(num)
+
+# for num in nums:
+#     for letter in 'abc':
+#         print(num, letter)
+
+x=0
+while x < 10:
+    # if x == 5:
+    #     break
+    print (x)
+    x +=1
